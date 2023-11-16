91cd4432a78f7328866c9bc34c76e3ede5f6f4c1
Anthony Ngo <anthony.g.ngo@gmail.com>
2023-11-15 22:49:16 -0800

    styling

--- script.js
+++ script.js
@@ -42,22 +42,31 @@ const updateBooksList = (list) => {
 	booksList.textContent = '';
 	list.forEach((book) => {
 		const bookDiv = document.createElement('div');
+		const bookMiddle = document.createElement('div');
 		const titleDiv = document.createElement('div');
 		const authorDiv = document.createElement('div');
 		const pagesDiv = document.createElement('div');
 		const readDiv = document.createElement('div');
+		const deleteBtn = document.createElement('button');
+
+		deleteBtn.textContent = 'X';
+		deleteBtn.classList.add = 'delete-btn';
 
 		const childrenArray = [titleDiv, authorDiv, pagesDiv, readDiv];
 
 		bookDiv.classList.add('book');
+		bookMiddle.classList.add('book-middle');
 		titleDiv.classList.add('book-title');
 		authorDiv.classList.add('book-author');
 		pagesDiv.classList.add('book-pages');
 		readDiv.classList.add('book-read');
 
 		childrenArray.forEach((child) => {
-			bookDiv.appendChild(child);
+			bookMiddle.appendChild(child);
 		});
+
+		bookDiv.appendChild(deleteBtn);
+		bookDiv.appendChild(bookMiddle);
 
 		titleDiv.textContent = book.title;
 		authorDiv.textContent = `By: ${book.author}`;
